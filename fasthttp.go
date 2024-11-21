53925e0cf4e7bfb109e0585cae06885bd5a7b8cf
Bozhidar <bobicloudvision@gmail.com>
2024-11-21 19:06:41 +0200

    update

--- fasthttp.go
+++ fasthttp.go
@@ -7,12 +7,13 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"io/ioutil"
 	"os"
 	"os/signal"
 // 	"strings"
 	"syscall"
 	"time"
-// 	"github.com/yookoala/gofast"
+	"github.com/yookoala/gofast"
 )
 
 type FastHTTPVirtualHost struct {
@@ -27,6 +28,7 @@ type FastHTTPVirtualHost struct {
     ErrorLog string `json:"errorLog"`
     CustomLog string `json:"customLog"`
     DirectoryIndex string `json:"directoryIndex"`
+    PHPProxyFCGI string `json:"phpProxyFCGI"`
 }
 
 type FastHTTPConfig struct {
@@ -34,7 +36,7 @@ type FastHTTPConfig struct {
 	Group string `json:"group"`
 	ServerAdmin string `json:"serverAdmin"`
 	Listen []string `json:"listen"`
-	virtualHosts []FastHTTPVirtualHost `json:"virtualHosts"`
+	VirtualHosts []FastHTTPVirtualHost `json:"virtualHosts"`
 	HttpPort string `json:"httpPort"`
 	HttpsPort string `json:"httpsPort"`
 }
@@ -57,9 +59,9 @@ func main() {
     }
 
     getVirtualHostByServerName := func(serverName string) *FastHTTPVirtualHost {
-        for i, v := range config.virtualHosts {
+        for i, v := range config.VirtualHosts {
           if v.ServerName == serverName {
-              return &config.virtualHosts[i]
+              return &config.VirtualHosts[i]
           }
       }
       return nil
@@ -77,66 +79,41 @@ func main() {
 
             virtualHost := getVirtualHostByServerName(r.Host)
             if virtualHost != nil {
-                log.Printf("Virtual host found: %s", virtualHost.ServerName)
-                log.Printf("Document root: %s", virtualHost.DocumentRoot)
+//                 log.Printf("Virtual host found: %s", virtualHost.ServerName)
+//                 log.Printf("Document root: %s", virtualHost.DocumentRoot)
 
-                http.FileServer(http.Dir(virtualHost.DocumentRoot)).ServeHTTP(w, r)
+//                 currentUri := r.RequestURI
+
+                isPHP := false
+                files, err := ioutil.ReadDir(virtualHost.DocumentRoot)
+                if err == nil {
+                    for _, file := range files {
+                        if file.Name() == "index.php" {
+                            isPHP = true
+                            break
+                        }
+                    }
+                }
+
+                if (isPHP && virtualHost.PHPProxyFCGI != "") {
+
+                    connFactory := gofast.SimpleConnFactory("tcp", virtualHost.PHPProxyFCGI)
+
+                    gfhandler := gofast.NewHandler(
+                        gofast.NewFileEndpoint(virtualHost.DocumentRoot + "/index.php")(gofast.BasicSession),
+                        gofast.SimpleClientFactory(connFactory),
+                    )
+
+                    http.HandlerFunc(gfhandler.ServeHTTP).ServeHTTP(w, r)
+
+                } else {
+                    http.FileServer(http.Dir(virtualHost.DocumentRoot)).ServeHTTP(w, r)
+                }
 
             } else {
                 log.Printf("Virtual host not found")
                 http.FileServer(http.Dir("/var/www/html")).ServeHTTP(w, r)
             }
-
-
-//
-//             if (r.Host == "adminbolt.com") {
-//
-//                 documentRoot := "/var/www/html"
-//                 http.FileServer(http.Dir(documentRoot)).ServeHTTP(w, r)
-//
-//                 log.Printf("adminbolt.com")
-//           } else if (r.Host == "vasil-levski.demo.adminbolt.com") {
-//
-//                 documentRoot2 := "/home/vasi96970cxn/public_html"
-//                 http.FileServer(http.Dir(documentRoot2)).ServeHTTP(w, r)
-//
-//           } else if (r.Host == "wordpress.demo.adminbolt.com") {
-
-//                 currentUri := r.RequestURI
-// //                 log.Printf("currentUri: %s", currentUri)
-//
-// //                 isPHP := false
-//                 isFile := false
-//                 fileExtensionList := []string{".php", ".html", ".htm", ".css", ".js", ".jpg", ".jpeg", ".png", ".gif", ".ico", ".svg", ".xml", ".json", ".txt", ".pdf", ".zip", ".gz", ".tar", ".rar", ".mp3", ".mp4", ".avi", ".mov", ".wmv", ".flv", ".webm", ".ogg", ".ogv", ".webp", ".woff", ".woff2", ".ttf", ".eot", ".otf", ".swf", ".fla", ".psd", ".ai", ".eps", ".indd", ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".odt", ".ods", ".odp", ".md", ".csv", ".sql", ".json", ".xml", ".yml", ".yaml", ".log", ".conf", ".ini", ".bak", ".tmp", ".temp", ".swp", ".swo", ".swn"}
-// //                 if currentUri == "/" {
-// //                     isPHP = true
-// //                 }
-//                 for _, ext := range fileExtensionList {
-//                     if strings.HasSuffix(currentUri, ext) {
-//                         isFile = true
-//                         break
-//                     }
-//                 }
-//
-//
-//                 documentRoot2 := "/home/word2442we7v/public_html"
-//
-//                 if (isFile == false) {
-//                     connFactory := gofast.SimpleConnFactory("tcp", "127.0.0.1:9076")
-//
-//                    gfhandler := gofast.NewHandler(
-//                         gofast.NewFileEndpoint(documentRoot2 + "/index.php")(gofast.BasicSession),
-//                         gofast.SimpleClientFactory(connFactory),
-//                     )
-//
-//                     http.HandlerFunc(gfhandler.ServeHTTP).ServeHTTP(w, r)
-//                 } else {
-//                     http.FileServer(http.Dir(documentRoot2)).ServeHTTP(w, r)
-//                 }
-//
-//           } else {
-//               fmt.Fprintf(w, "Hello, %q! Host: %s", html.EscapeString(r.URL.Path))
-//           }
 
 		}),
 	}
